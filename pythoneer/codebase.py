f0916b294cb27d3d273d9c6ff39c1758e143855f
tomwetherell <tomwetherell@icloud.com>
2024-06-28 19:56:23 +0100

    Use relative file path when adding source files to codebase

--- pythoneer/codebase.py
+++ pythoneer/codebase.py
@@ -27,13 +27,13 @@ class Codebase:
 
         # Add all source files in the codebase to the codebase object
         for file_path in self.codebase_path.glob(self.PATTERN):
-            self.add_file(file_path)
+            relative_file_path = str(file_path.relative_to(self.codebase_path))
+            self.add_file(relative_file_path)
 
-    def add_file(self, file_path: str | Path):
+    def add_file(self, relative_file_path: str):
         """Add a new source file to the codebase."""
-        file_path = Path(file_path)
-        source_file = SourceFile(self.codebase_path, file_path)
-        self.files[source_file.relative_file_path] = source_file
+        source_file = SourceFile(self.codebase_path, relative_file_path)
+        self.files[relative_file_path] = source_file
 
     def retrieve_file(self, relative_file_path: str) -> SourceFile:
         """Retrieve a SourceFile object from the codebase."""
@@ -50,7 +50,7 @@ class SourceFile:
     def __init__(
         self,
         codebase_path: str | Path,
-        file_path: str | Path,
+        relative_file_path: str,
     ):
         """
         Initalise the SourceFile object.
@@ -60,13 +60,12 @@ class SourceFile:
         codebase_path : str | Path
             Full path to the root of the codebase.
 
-        file_path : str | Path
-            Full path to the source file.
+        relative_file_path : str
+            Path of the source file relative to the root of the codebase.
         """
         self.codebase_path = Path(codebase_path)
-        self.file_path = Path(file_path)
-
-        self._relative_file_path = str(self.file_path.relative_to(self.codebase_path))
+        self._relative_file_path = relative_file_path
+        self.file_path = self.codebase_path / self.relative_file_path
         self._file_name = self.file_path.name
 
         self.versions = []
